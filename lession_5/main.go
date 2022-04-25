b2f3c1b28d99769a4800f44ee875dce54e5ec240
Vadim Popov <popov.v@soft2bet.com>
2022-04-25 20:09:44 +0400

    feat: add cache for result

--- lession_5/main.go
+++ lession_5/main.go
@@ -6,19 +6,30 @@ import "fmt"
 // 3. Проверьте себя: вам должны быть знакомы следующие ключевые слова Go: func, return, defer, struct, type.
 // 4. Посмотрите задачи из предыдущих уроков. Как можно улучшить дизайн задач? Что бы вы разбили на отдельные функции или даже пакеты?
 
+var cache map[int]int = make(map[int]int)
+
 func main() {
 	var a int
-	fmt.Print("Введите порядковый номер: ")
+	fmt.Println("Введите порядковый номер: ")
 	fmt.Scanln(&a)
 	fmt.Printf("Результат: %v", fib(a))
 }
 
 func fib(n int) int {
+    if cache[n] != 0 {
+		return cache[n]
+	}
     if n == 0 {
-      return 0
+    	return 0
     }
     if n == 1 {
-      return 1
+    	return 1
     }
-    return fib(n - 1) + fib(n - 2)
+    a := fib(n - 1)
+	b := fib(n - 2)
+
+	cache[n-1] = a
+	cache[n-2] = b
+
+    return a + b
 }
